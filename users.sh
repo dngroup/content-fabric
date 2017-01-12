714d4f1897a3c9cecafa507023cd5894eb6be226
dbourasseau <dbourasseau@viotech.net>
2017-01-12 14:06:00 +0100

    add docker-compose  and exemple to start a users

---
+++ users.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+.user/user -events-address=172.18.0.2:7053 -listen-to-rejections=true -events-from-chaincode=9cef66637fefd8ff24c8907e220e8d4ff51d47587e6b2b38e72b1944723d18fee371a4fa5cd2eecdc8e45f1b93f650595c7e5168fd32e7cb2118786db2a95be8 -value-to-analyse=a -value-to-change=b -rest-address=172.18.0.2:7050
