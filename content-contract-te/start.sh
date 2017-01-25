3dc2f9588c5816b8d33ba0c4787e4471e6016d48
Nicolas Herbaut <nherbaut@labri.fr>
2017-01-25 17:35:29 +0100

    added ip to start

--- content-contract-te/start.sh
+++ content-contract-te/start.sh
@@ -4,7 +4,7 @@ do
     docker rm -f te${number}
 done
 if [ "$#" -ne 4 ]; then
-   echo "./start NUMBEROFCP PERCENTOFCHANCETOHAVECONTENT PERCENTOFCHANCEFORPRICELOWER CONTRACTID"
+   echo "./start NUMBEROFCP PERCENTOFCHANCETOHAVECONTENT PERCENTOFCHANCEFORPRICELOWER CONTRACTID PeerIP"
 
    exit -1
 fi
@@ -12,5 +12,5 @@ fi
 for ((number=1; number<=$1;number++))
 do
 #    docker rm -f te${number}
-    docker run --name te${number} -d -e PEERADDR=172.17.0.1 -e CP-ID=cp${number} -e CHAINCODE=$4 -e PERCENT=$2 -e PERCENTPRICE=$3 dngroup/content-contract-te
+    docker run --name te${number} -d -e PEERADDR=$5 -e CP-ID=cp${number} -e CHAINCODE=$4 -e PERCENT=$2 -e PERCENTPRICE=$3 dngroup/content-contract-te
 done
