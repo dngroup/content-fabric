a850faded3f724a509b3d6d09623943c117e6157
dbourasseau <dbourasseau@viotech.net>
2017-02-02 15:57:36 +0100

    add parameter for inputgenerator

--- inputgenerator.py
+++ inputgenerator.py
@@ -19,18 +19,13 @@ CONSENSUS_TIME_MAX = "100ms"
 
 PEER_COUNT=4
 
-columns = ["peer_count",
-           "client_count",
-           "arrival_time",
-           "te_count",
-           "cp_count",
-           "te_percent",
-           "te_percent_price",
-           "cp_percent",
-           "consensus",
-           "consensus_time_max",
-           "do"
-           ]
+TE_PERCENT = 100
+TE_PERCENT_PRICE = 100
+CP_PERCENT = 100
+CONSENSUS = "pbft"
+
+CP_COUNT = 10
+
 
 
 parser = argparse.ArgumentParser(description='', epilog=
@@ -45,6 +40,15 @@ parser.add_argument('--tes', type=int, help='number of value cp and te', default
 parser.add_argument('--max_te', type=int, help='max number of  cp and te', default=MAX_TE)
 parser.add_argument('--min_te', type=int, help='min number of cp and te', default=MIN_TE)
 parser.add_argument('--consensus_time_max', help='time max before consensus ', default=CONSENSUS_TIME_MAX)
+parser.add_argument('--te_percent', type=int, help='percent of change to have content in a TE', default=TE_PERCENT)
+parser.add_argument('--te_percent_price', type=int,
+                    help='percent of change to a better price thant the max price fixed by the CP',
+                    default=TE_PERCENT_PRICE)
+parser.add_argument('--cp_percent', type=int, help='percent of change to have content licensing in a CP',
+                    default=CP_PERCENT)
+parser.add_argument('--cp_count', type=int, help='Number of Content providers (need 1 or more)', default=CP_COUNT)
+parser.add_argument('--consensus', help='consensus to use default pbft', default=CONSENSUS)
+
 
 args = parser.parse_args()
 MAX_USER=args.max_user
@@ -55,8 +59,23 @@ MAX_TE=args.max_te
 MIN_TE=args.min_te
 PEER_COUNT=args.peer_count
 CONSENSUS_TIME_MAX = args.consensus_time_max
+TE_PERCENT = args.te_percent
+TE_PERCENT_PRICE = args.te_percent_price
+CP_PERCENT = args.cp_percent
+CONSENSUS = args.consensus
 
-
+columns = ["peer_count",
+           "client_count",
+           "arrival_time",
+           "te_count",
+           "cp_count",
+           "te_percent",
+           "te_percent_price",
+           "cp_percent",
+           "consensus",
+           "consensus_time_max",
+           "do"
+           ]
 
 
 users=np.geomspace(MIN_USER,MAX_USER,USERS)
@@ -65,10 +84,15 @@ print ","+",".join(columns)
 for user in users:
     for te in cptes:
         print ("0,%d ,%d , %.2f, %d ,%d , %d , %d , %d , %s , %s, False" % (
-            PEER_COUNT, user, 60.0 / user, te, 10,
-            100,
-            100,
-            100,
-            "noops",CONSENSUS_TIME_MAX
+            PEER_COUNT,
+            user,
+            60.0 / user,
+            te,
+            CP_COUNT,
+            TE_PERCENT,
+            TE_PERCENT_PRICE,
+            CP_PERCENT,
+            CONSENSUS,
+            CONSENSUS_TIME_MAX
             ))
 
